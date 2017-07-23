acda9f63c3b9e32cc86d54378d4fe20a2df45996
Brian Stitt <brianstitt@live.com>
2017-07-23 09:08:43 -0400

    Added documentation to PhantomJS scripts

--- www/js/PhantomJS_webpage_screenshot.js
+++ www/js/PhantomJS_webpage_screenshot.js
@@ -6,3 +6,11 @@ page.open('http://google.com', function(status) {
   }
   phantom.exit();
 });
+/*To run this commmand open a terminal window,
+navigate to the directory where the JS file lives
+and type:
+
+  PhantomJS_webpage_screenshot.js
+
+  The script will open http://google.com, and take a picture of the webpage
+  and save it as exmample.png

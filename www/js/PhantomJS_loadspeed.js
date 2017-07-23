67d59754db4a87ad3306dc3bc1c2f8987bd8f7a8
Brian Stitt <brianstitt@live.com>
2017-07-23 09:06:17 -0400

    Added PhantomJS scripts

---
+++ www/js/PhantomJS_loadspeed.js
@@ -0,0 +1,27 @@
+var page = require('webpage').create(),
+  system = require('system'),
+  t, address;
+
+if (system.args.length === 1) {
+  console.log('Usage: loadspeed.js <some URL>');
+  phantom.exit();
+}
+
+t = Date.now();
+address = system.args[1];
+page.open(address, function(status) {
+  if (status !== 'success') {
+    console.log('FAIL to load the address');
+  } else {
+    t = Date.now() - t;
+    console.log('Loading ' + system.args[1]);
+    console.log('Loading time ' + t + ' msec');
+  }
+  phantom.exit();
+});
+
+/*To urun this commmand open a terminal window,
+navigate t to the directory where the JS file lives
+and type:
+
+  PhantomJS_loadspeed.js http://www.google.com
